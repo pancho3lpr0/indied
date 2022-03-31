d9e3046787fcffb05bd4c2b4ecfc38be65df0ff2
Francisco Peña <penf8544gmail.com>
2022-03-31 13:09:16 -0600

    ya mero x6

--- indeedjfpp/asgi.py
+++ indeedjfpp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for indeed_jfpp project.
+ASGI config for indeedjfpp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indeed_jfpp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indeedjfpp.settings')
 
 application = get_asgi_application()
--- indeedjfpp/wsgi.py
+++ indeedjfpp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for indeed_jfpp project.
+WSGI config for indeedjfpp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indeed_jfpp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indeedjfpp.settings')
 
 application = get_wsgi_application()
