1588182d869a81154779a5b5fbbff8e438a2899e
Francisco Peña <penf8544gmail.com>
2022-04-07 12:23:30 -0500

    ya merito 28

--- appindeed/forms.py
+++ appindeed/forms.py
@@ -18,7 +18,7 @@ class loginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'remember_me']
-    user_name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
+    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
     password = forms.CharField(max_length=50, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control', 'data-toggle': 'password', 'id': 'password', 'name': 'password'}))
     remember_me = forms.BooleanField(required=False)
     
